82f01293c2fe297034b69534e4ebf9261901a824
Oleksandr1987 <solovij1987@gmail.com>
2025-03-28 17:21:08 +0200

    Remove dob from client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_18_103833) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_28_151630) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,9 +42,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_18_103833) do
   create_table "clients", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "last_name"
     t.string "phone", null: false
-    t.date "birthdate"
     t.string "hair_type"
     t.string "hair_length"
     t.string "hair_structure"
