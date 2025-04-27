56aac652e37c110b933f9c5469a64b95f1972fba
Oleksandr1987 <solovij1987@gmail.com>
2025-04-27 15:53:48 +0300

    Add admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/admin', to: 'admin#index', as: :admin
+
   root to: "home#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
