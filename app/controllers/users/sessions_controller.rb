7cdd86009f80c5e518e8e5cd65955b8a4871d9ad
Oleksandr1987 <solovij1987@gmail.com>
2025-11-18 17:16:57 +0200

    Improve login logic

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :normalize_login_phone, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_lacation_for(resource_or_scope) || root_path
+    calendar_appointments_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    stored_lacation_for(resource_or_scope) || root_path
+    calendar_appointments_path
   end
 
   # GET /resource/sign_in
@@ -29,7 +30,18 @@ class Users::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def normalize_login_phone
+    return if params[:user].blank? || params[:user][:login].blank?
+
+    login = params[:user][:login]
+
+    unless login.include?("@")
+      normalized = PhoneValidator.normalize(login)
+      params[:user][:login] = normalized
+    end
+  end
+
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   include PhoneValidator
 
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,20 +17,35 @@ class User < ApplicationRecord
   has_many :services, dependent: :destroy
   has_many :expenses, dependent: :destroy
 
-  def self.from_omniauth(auth)
-    if where(email: auth.info.email).exists?
-      return_user = where(email: auth.info.email).first
-      return_user.provider = auth.provider
-      return_user.uid = auth.uid
-    else
-      return_user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0, 20]
-        user.name = auth.info.name
-        user.phone = auth.info.phone || ""
+  class << self
+    def find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)&.strip
+
+      if login.present? && !login.include?("@")
+        login = PhoneValidator.normalize(login)
       end
+
+      where(conditions).where(
+        ["LOWER(email) = :value OR phone = :value", { value: login.downcase }]
+      ).first
     end
-    return_user
+
+    def from_omniauth(auth)
+      if where(email: auth.info.email).exists?
+        return_user = where(email: auth.info.email).first
+        return_user.provider = auth.provider
+        return_user.uid = auth.uid
+      else
+        return_user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+          user.email = auth.info.email
+          user.password = Devise.friendly_token[0, 20]
+          user.name = auth.info.name
+          user.phone = auth.info.phone || ""
+        end
+      end
+      return_user
+    end
   end
 
   def has_active_subscription?
