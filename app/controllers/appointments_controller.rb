e7145fd5bcd84b971ed7d7db3e6bbf93df821b73
Oleksandr1987 <solovij1987@gmail.com>
2026-04-06 14:32:39 +0300

    Clean up

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -105,17 +105,12 @@ class AppointmentsController < ApplicationController
 
     slots = Appointment.available_slots(current_user, date)
 
-    Rails.logger.debug "----- JSON SLOTS -----"
-  slots.each do |s|
-    Rails.logger.debug "slot json start: #{s[:start]}"
-  end
-    render json: slots.map {
-      |slot|
+    render json: slots.map do |slot|
       {
         start: slot[:start].strftime("%H:%M"),
         end: slot[:end].strftime("%H:%M")
       }
-    }
+    end
   end
 
   private
